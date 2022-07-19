ece7f48c419149752dc1428e19d397a449b57393
Maxim Moinat <maxim@thehyve.nl>
2022-07-19 13:54:28 +0200

    add visit occurrence to concepts per person. Fixes #7

--- inst/sql/sql_server/checks/conceptsperperson.sql
+++ inst/sql/sql_server/checks/conceptsperperson.sql
@@ -1,95 +1,22 @@
 SELECT
-  'Condition occurrence' AS category,
-  ard1.min_Value          AS min_Value,
-  ard1.p10_Value          AS p10_Value,
-  ard1.p25_Value          AS p25_Value,
-  ard1.median_Value       AS median_Value,
-  ard1.p75_Value          AS p75_Value,
-  ard1.p90_Value          AS p90_Value,
-  ard1.max_Value          AS max_Value
-FROM @resultsDatabaseSchema.achilles_results_dist ard1
-WHERE ard1.analysis_id = 403
-
-UNION
-
-SELECT
-  'Procedure occurrence' AS category,
-  ard1.min_Value          AS min_Value,
-  ard1.p10_Value          AS p10_Value,
-  ard1.p25_Value          AS p25_Value,
-  ard1.median_Value       AS median_Value,
-  ard1.p75_Value          AS p75_Value,
-  ard1.p90_Value          AS p90_Value,
-  ard1.max_Value          AS max_Value
-FROM @resultsDatabaseSchema.achilles_results_dist ard1
-WHERE ard1.analysis_id = 603
-
-UNION
-
-SELECT
-  'Drug exposure'  AS category,
-  ard1.min_Value    AS min_Value,
-  ard1.p10_Value    AS p10_Value,
-  ard1.p25_Value    AS p25_Value,
-  ard1.median_Value AS median_Value,
-  ard1.p75_Value    AS p75_Value,
-  ard1.p90_Value    AS p90_Value,
-  ard1.max_Value    AS max_Value
-FROM @resultsDatabaseSchema.achilles_results_dist ard1
-WHERE ard1.analysis_id = 703
-
-UNION
-
-SELECT
-  'Observation'    AS category,
-  ard1.min_Value    AS min_Value,
-  ard1.p10_Value    AS p10_Value,
-  ard1.p25_Value    AS p25_Value,
-  ard1.median_Value AS median_Value,
-  ard1.p75_Value    AS p75_Value,
-  ard1.p90_Value    AS p90_Value,
-  ard1.max_Value    AS max_Value
-FROM @resultsDatabaseSchema.achilles_results_dist ard1
-WHERE ard1.analysis_id = 803
-
-UNION
-
-SELECT
-  'Drug era'       AS category,
-  ard1.min_Value    AS min_Value,
-  ard1.p10_Value    AS p10_Value,
-  ard1.p25_Value    AS p25_Value,
-  ard1.median_Value AS median_Value,
-  ard1.p75_Value    AS p75_Value,
-  ard1.p90_Value    AS p90_Value,
-  ard1.max_Value    AS max_Value
-FROM @resultsDatabaseSchema.achilles_results_dist ard1
-WHERE ard1.analysis_id = 903
-
-UNION
-
-SELECT
-  'Condition era'  AS category,
-  ard1.min_Value    AS min_Value,
-  ard1.p10_Value    AS p10_Value,
-  ard1.p25_Value    AS p25_Value,
-  ard1.median_Value AS median_Value,
-  ard1.p75_Value    AS p75_Value,
-  ard1.p90_Value    AS p90_Value,
-  ard1.max_Value    AS max_Value
-FROM @resultsDatabaseSchema.achilles_results_dist ard1
-WHERE ard1.analysis_id = 1003
-
-UNION
-
-SELECT
-  'Measurement'  AS category,
-  ard1.min_Value    AS min_Value,
-  ard1.p10_Value    AS p10_Value,
-  ard1.p25_Value    AS p25_Value,
-  ard1.median_Value AS median_Value,
-  ard1.p75_Value    AS p75_Value,
-  ard1.p90_Value    AS p90_Value,
-  ard1.max_Value    AS max_Value
-FROM @resultsDatabaseSchema.achilles_results_dist ard1
-WHERE ard1.analysis_id = 1803
+  CASE analysis_id
+    WHEN 203 THEN 'Visit Occurrence'
+    WHEN 403 THEN 'Condition Occurrence'
+    WHEN 603 THEN 'Procedure Occurrence'
+    WHEN 703 THEN 'Drug exposure'
+    WHEN 803 THEN 'Observation'
+    WHEN 903 THEN 'Drug era'
+    WHEN 1003 THEN 'Condition era'
+    WHEN 1803 THEN 'Measurement'
+    ELSE ''
+  END AS             AS category,
+  min_Value          AS min_Value,
+  p10_Value          AS p10_Value,
+  p25_Value          AS p25_Value,
+  median_Value       AS median_Value,
+  p75_Value          AS p75_Value,
+  p90_Value          AS p90_Value,
+  max_Value          AS max_Value
+FROM @resultsDatabaseSchema.achilles_results_dist
+WHERE analysis_id IN (203, 403, 603, 703, 803, 903, 1003, 1803)
+;
