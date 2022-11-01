5430e3d4900c6a972a0ac1c4f34e5aea170f08b3
Maxim Moinat <maximmoinat@gmail.com>
2022-11-01 13:24:16 +0100

    fix mapping completeness

--- inst/sql/sql_server/checks/mapping_completeness.sql
+++ inst/sql/sql_server/checks/mapping_completeness.sql
@@ -7,7 +7,7 @@ select 'Condition' as "Domain",
         100.0*sum(case when is_mapped > 0 then num_records else 0 end)/sum(num_records) as "%Records Mapped"
 from
 (
-select condition_source_value, case when condition_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
+select case when condition_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
 from @cdmDatabaseSchema.condition_occurrence
 group by condition_source_value, case when condition_concept_id > 0 then 1 else 0 end
 ) T
@@ -22,7 +22,7 @@ select 'Procedure', count_big(*),
         100.0*sum(case when is_mapped > 0 then num_records else 0 end)/sum(num_records)
 from
 (
-select procedure_source_value, case when procedure_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
+select case when procedure_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
 from @cdmDatabaseSchema.procedure_occurrence
 group by procedure_source_value, case when procedure_concept_id > 0 then 1 else 0 end
 ) T
@@ -37,7 +37,7 @@ select 'Device', count_big(*),
         100.0*sum(case when is_mapped > 0 then num_records else 0 end)/sum(num_records)
 from
 (
-select device_source_value, case when device_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
+select case when device_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
 from @cdmDatabaseSchema.device_exposure
 group by device_source_value, case when device_concept_id > 0 then 1 else 0 end
 ) T
@@ -53,7 +53,7 @@ select 'Drug', count_big(*),
         100.0*sum(case when is_mapped > 0 then num_records else 0 end)/sum(num_records)
 from
 (
-select drug_source_value, case when drug_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
+select case when drug_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
 from @cdmDatabaseSchema.drug_exposure
 group by drug_source_value, case when drug_concept_id > 0 then 1 else 0 end
 ) T
@@ -70,7 +70,7 @@ select 'Observation', count_big(*),
         100.0*sum(case when is_mapped > 0 then num_records else 0 end)/sum(num_records)
 from
 (
-select observation_source_value, case when observation_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
+select case when observation_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
 from @cdmDatabaseSchema.observation
 group by observation_source_value, case when observation_concept_id > 0 then 1 else 0 end
 ) T
@@ -87,7 +87,7 @@ select 'Measurement', count_big(*),
         100.0*sum(case when is_mapped > 0 then num_records else 0 end)/sum(num_records)
 from
 (
-select measurement_source_value, case when measurement_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
+select case when measurement_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
 from @cdmDatabaseSchema.measurement
 group by measurement_source_value, case when measurement_concept_id > 0 then 1 else 0 end
 ) T
@@ -102,7 +102,7 @@ select 'Visit', count_big(*),
         100.0*sum(case when is_mapped > 0 then num_records else 0 end)/sum(num_records)
 from
 (
-select visit_source_value, case when visit_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
+select case when visit_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
 from @cdmDatabaseSchema.visit_occurrence
 group by visit_source_value, case when visit_concept_id > 0 then 1 else 0 end
 ) T
@@ -117,7 +117,7 @@ select 'Measurement unit', count_big(*),
       100.0*sum(case when is_mapped > 0 then num_records else 0 end)/sum(num_records)
 from
 (
-   select unit_source_value, case when unit_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
+   select case when unit_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
    from @cdmDatabaseSchema.measurement
    where unit_concept_id IS NOT NULL
    group by unit_source_value, case when unit_concept_id > 0 then 1 else 0 end
@@ -133,7 +133,7 @@ select 'Observation unit', count_big(*),
       100.0*sum(case when is_mapped > 0 then num_records else 0 end)/sum(num_records)
 from
 (
-   select unit_source_value, case when unit_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
+   select case when unit_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
    from @cdmDatabaseSchema.observation
    where unit_concept_id IS NOT NULL
    group by unit_source_value, case when unit_concept_id > 0 then 1 else 0 end
@@ -149,7 +149,7 @@ select 'Measurement value', count_big(*),
       100.0*sum(case when is_mapped > 0 then num_records else 0 end)/sum(num_records)
 from
 (
-   select value_source_value, case when value_as_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
+   select case when value_as_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
    from @cdmDatabaseSchema.measurement
    where value_as_concept_id IS NOT NULL
    group by value_source_value, case when value_as_concept_id > 0 then 1 else 0 end
@@ -165,7 +165,7 @@ select 'Observation value', count_big(*),
       100.0*sum(case when is_mapped > 0 then num_records else 0 end)/sum(num_records)
 from
 (
-   select '', case when value_as_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
+   select case when value_as_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
    from @cdmDatabaseSchema.observation
    where value_as_concept_id IS NOT NULL
    group by case when value_as_concept_id > 0 then 1 else 0 end
@@ -181,7 +181,7 @@ select 'Provider Specialty', count_big(*),
       100.0*sum(case when is_mapped > 0 then num_records else 0 end)/sum(num_records)
 from
 (
-   select specialty_source_value, case when specialty_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
+   select case when specialty_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
    from @cdmDatabaseSchema.provider
    where specialty_concept_id IS NOT NULL
    group by specialty_source_value, case when specialty_concept_id > 0 then 1 else 0 end
@@ -197,7 +197,7 @@ select 'Specimen', count_big(*),
       100.0*sum(case when is_mapped > 0 then num_records else 0 end)/sum(num_records)
 from
 (
-   select specimen_source_value, case when specimen_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
+   select case when specimen_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
    from @cdmDatabaseSchema.specimen
    where specimen_concept_id IS NOT NULL
    group by specimen_source_value, case when specimen_concept_id > 0 then 1 else 0 end
@@ -213,7 +213,7 @@ select 'Death cause', count_big(*),
       100.0*sum(case when is_mapped > 0 then num_records else 0 end)/sum(num_records)
 from
 (
-   select cause_source_value, case when cause_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
+   select case when cause_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
    from @cdmDatabaseSchema.death
    where cause_concept_id IS NOT NULL
    group by cause_source_value, case when cause_concept_id > 0 then 1 else 0 end
@@ -229,7 +229,7 @@ select 'Condition status', count_big(*),
       100.0*sum(case when is_mapped > 0 then num_records else 0 end)/sum(num_records)
 from
 (
-   select condition_status_source_value, case when condition_status_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
+   select case when condition_status_concept_id > 0 then 1 else 0 end as is_mapped, count_big(*) as num_records
    from @cdmDatabaseSchema.condition_occurrence
    where condition_status_concept_id IS NOT NULL
    group by condition_status_source_value, case when condition_status_concept_id > 0 then 1 else 0 end
