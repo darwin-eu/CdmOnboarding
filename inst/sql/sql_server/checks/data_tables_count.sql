6b5d9f76b050133a556b88b6cebb26fb04dff978
Maxim Moinat <maximmoinat@gmail.com>
2022-11-08 13:21:57 +0100

    use union all

--- inst/sql/sql_server/checks/data_tables_count.sql
+++ inst/sql/sql_server/checks/data_tables_count.sql
@@ -1,49 +1,49 @@
 -- Clinical data table counts
 
 select 'person' as tablename, count_big(*) as count from @cdmDatabaseSchema.person
-UNION
+UNION ALL
 select 'care_site' as tablename, count_big(*) as count from @cdmDatabaseSchema.care_site
-UNION
+UNION ALL
 select 'condition_era' as tablename, count_big(*) as count from @cdmDatabaseSchema.condition_era
-UNION
+UNION ALL
 select 'condition_occurrence' as tablename, count_big(*) as count from @cdmDatabaseSchema.condition_occurrence
-UNION
+UNION ALL
 select 'drug_exposure' as tablename, count_big(*) as count from @cdmDatabaseSchema.drug_exposure
-UNION
+UNION ALL
 select 'cost' as tablename, count_big(*) as count from @cdmDatabaseSchema.cost
-UNION
+UNION ALL
 select 'death' as tablename, count_big(*) as count from @cdmDatabaseSchema.death
-UNION
+UNION ALL
 select 'device_exposure' as tablename, count_big(*) as count from @cdmDatabaseSchema.device_exposure
-UNION
+UNION ALL
 select 'dose_era' as tablename, count_big(*) as count from @cdmDatabaseSchema.dose_era
-UNION
+UNION ALL
 select 'drug_era' as tablename, count_big(*) as count from @cdmDatabaseSchema.drug_era
-UNION
+UNION ALL
 select 'location' as tablename, count_big(*) as count from @cdmDatabaseSchema.location
-UNION
+UNION ALL
 select 'measurement' as tablename, count_big(*) as count from @cdmDatabaseSchema.measurement
-UNION
+UNION ALL
 select 'note' as tablename, count_big(*) as count from @cdmDatabaseSchema.note
-UNION
+UNION ALL
 select 'observation' as tablename, count_big(*) as count from @cdmDatabaseSchema.observation
-UNION
+UNION ALL
 select 'observation_period' as tablename, count_big(*) as count from @cdmDatabaseSchema.observation_period
-UNION
+UNION ALL
 select 'payer_plan_period' as tablename, count_big(*) as count from @cdmDatabaseSchema.payer_plan_period
-UNION
+UNION ALL
 select 'procedure_occurrence' as tablename, count_big(*) as count from @cdmDatabaseSchema.procedure_occurrence
-UNION
+UNION ALL
 select 'provider' as tablename, count_big(*) as count from @cdmDatabaseSchema.provider
-UNION
+UNION ALL
 select 'specimen' as tablename, count_big(*) as count from @cdmDatabaseSchema.specimen
-UNION
+UNION ALL
 select 'visit_detail' as tablename, count_big(*) as count from @cdmDatabaseSchema.visit_detail
-UNION
+UNION ALL
 select 'visit_occurrence' as tablename, count_big(*) as count from @cdmDatabaseSchema.visit_occurrence
 {@cdmVersion == '5.4'} ? {
-UNION
+UNION ALL
 select 'episode' as tablename, count_big(*) as count from @cdmDatabaseSchema.episode
-UNION
+UNION ALL
 select 'episode_event' as tablename, count_big(*) as count from @cdmDatabaseSchema.episode_event
 }
