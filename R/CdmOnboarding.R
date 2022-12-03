a45f61854142d7d9feebed113ca5b4fef84b61d3
Maxim Moinat <maximmoinat@gmail.com>
2022-12-03 15:45:48 +0100

    bugfix in type concepts, log metadata at execution start

--- R/CdmOnboarding.R
+++ R/CdmOnboarding.R
@@ -185,6 +185,7 @@ cdmOnboarding <- function(connectionDetails,
   ParallelLogger::registerLogger(logger)
 
   start_time <- Sys.time()
+  ParallelLogger::logInfo(sprintf('Running CdmOnboarding v%s %s', packageVersion("CdmOnboarding"), if(optimize) {"(performance optimized)"} else {""}))
 
   # CDM Source ------------------------------------------
   cdmSource <- .getCdmSource(connectionDetails, cdmDatabaseSchema, sqlOnly, outputFolder)
@@ -195,6 +196,7 @@ cdmOnboarding <- function(connectionDetails,
   cdmSource$CDM_RELEASE_DATE <- as.character(cdmSource$CDM_RELEASE_DATE)
   cdmSource$SOURCE_RELEASE_DATE <- as.character(cdmSource$SOURCE_RELEASE_DATE)
   cdmVersion <- gsub(pattern = "v", replacement = "", cdmSource$CDM_VERSION)
+  ParallelLogger::logInfo(sprintf("Found database '%s' with release date '%s'", cdmSource$CDM_SOURCE_NAME, cdmSource$CDM_RELEASE_DATE)
 
   # Get source name from cdm_source if none provided ----------------------------------------------------------------------------------------------
   if (missing(databaseName) & !sqlOnly) {
