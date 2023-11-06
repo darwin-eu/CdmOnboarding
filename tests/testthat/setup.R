792372b35889c991c218eed706a0258e1ef67e7b
Maxim Moinat <maximmoinat@gmail.com>
2023-11-06 10:47:17 +0100

    wip check achilles results exists

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,9 +1,13 @@
-  library(Eunomia)
-  connectionDetails <- Eunomia::getEunomiaConnectionDetails()
+library(Eunomia)
+connectionDetails <- Eunomia::getEunomiaConnectionDetails()
 
+hasAchillesResults <- CdmOnboarding:::.checkAchillesTablesExist(connectionDetails, "main", testthat::test_path())
+
+if (!hasAchillesResults) {
   Achilles::achilles(
     connectionDetails = connectionDetails,
     cdmDatabaseSchema = "main",
     resultsDatabaseSchema = "main",
     analysisIds = c(0, 105, 110, 111, 117, 220, 420, 502, 620, 720, 820, 920, 1020, 1820, 2102, 2120, 203, 403, 603, 703, 803, 903, 920, 1003, 1020, 1320, 1411, 1803, 1820)
   )
+}
