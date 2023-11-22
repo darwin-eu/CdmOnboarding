f1307e78bdd78f6e04ac083463b28129f92c1ae0
Maxim Moinat <maximmoinat@gmail.com>
2023-11-22 21:36:20 +0100

    update active persons query to work with sqlite

--- inst/sql/sql_server/checks/active_persons.sql
+++ inst/sql/sql_server/checks/active_persons.sql
@@ -2,5 +2,5 @@ select
     count(distinct person_id) AS count
 from @cdmDatabaseSchema.observation_period
 cross join @cdmDatabaseSchema.cdm_source
-where observation_period_end_date >= DATEADD(month, -6, coalesce(source_release_date, cdm_release_date))
+where observation_period_end_date >= DATEADD(m, -6, coalesce(source_release_date, cdm_release_date))
 ;
