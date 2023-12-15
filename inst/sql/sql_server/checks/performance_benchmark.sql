40623b52113ccd4db16f8c36019e7aed02b523d2
Maxim Moinat <maximmoinat@gmail.com>
2023-12-15 15:57:33 +0100

    fix performance_benchmark

--- inst/sql/sql_server/checks/performance_benchmark.sql
+++ inst/sql/sql_server/checks/performance_benchmark.sql
@@ -7,7 +7,7 @@ JOIN @cdmDatabaseSchema.concept_relationship AS cr
   ON c1.concept_id = concept_id_1
   AND cr.invalid_reason IS NULL
   AND cr.relationship_id = 'Maps to'
-JOIN @vocabDatabaseSchema.concept c2
+JOIN @cdmDatabaseSchema.concept c2
   ON cr.concept_id_2 = c2.concept_id
   AND c2.invalid_reason IS NULL
 ;
