8cef1dc465f65ada112c4bd505bdd166c64921aa
Maxim Moinat <maximmoinat@gmail.com>
2024-06-06 11:08:54 +0200

    remove dependency on ROhdsiWebApi

--- R/CdmOnboarding-package.R
+++ R/CdmOnboarding-package.R
@@ -1,9 +1,8 @@
-#' @importFrom utils compareVersion packageVersion read.csv zip installed.packages
+#' @import utils
 #' @import dplyr
 #' @import officer
 #' @import ggplot2
 #' @import DatabaseConnector
 #' @import ParallelLogger
 #' @import SqlRender
-#' @import ROhdsiWebApi
 NULL
