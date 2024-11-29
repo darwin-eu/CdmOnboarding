ae58f9b79042ed460c55fe37da30bc242faab08d
Maxim Moinat <maximmoinat@gmail.com>
2024-11-29 15:52:41 +0100

    update test setup

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,10 +1,19 @@
-# Using CDMConnector dataset with Achilles results pre-loaded
-# Note: temp database is stored at Sys.getenv("EUNOMIA_DATA_FOLDER")
-library(CDMConnector)
-library(DatabaseConnector)
+# Setup for using CDMConnector dataset with Achilles results pre-loaded
+
+withr::local_envvar(
+  R_USER_CACHE_DIR = tempfile(),
+  .local_envir = teardown_env(),
+  EUNOMIA_DATA_FOLDER = Sys.getenv("EUNOMIA_DATA_FOLDER", unset = tempfile())
+)
 
 datasetName <- "synpuf-1k"
 cdmVersion <- "5.3"
+
+tryCatch(
+  if (Sys.getenv("skip_eunomia_download_test") != "TRUE") downloadEunomiaData(datasetName, cdmVersion, overwrite = TRUE),
+  error = function(e) NA
+)
+
 server <- CDMConnector::eunomiaDir(datasetName, cdmVersion)
 
 params <- list(
